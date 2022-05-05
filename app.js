a60c1a0c267a9c52fee3f7cf8d125a29c9320965
ManuelHMR <manuelhmrjf@gmail.com>
2022-05-04 23:11:43 -0300

    basic endpoints organization

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 import express from "express";
+import cors from "cors"; //DELETAR NO DEPLOY
+
+import db from "./db.js";
 
 const app = express();
-
 app.listen(5000);
+app.use(express.json());
+app.use(cors()); // DELETAR NO DEPLOY
+
+app.post("/sign-in")
+app.post("/sign-up")
+app.get("/balance")
+app.post("/balance")
+app.delete("/balance")
+app.put("/balance")
