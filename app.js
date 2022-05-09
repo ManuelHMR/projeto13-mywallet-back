b9a12ef6461c6b621edbff31186617b90301fc45
ManuelHMR <manuelhmrjf@gmail.com>
2022-05-09 16:21:57 -0300

    make it better

--- app.js
+++ app.js
@@ -11,9 +11,18 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
-app.listen(port);
-app.use(express.json());
+
 app.use(cors()); // DELETAR NO DEPLOY
+app.use(express.json());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
 
 app.use(sessionRouter);
 app.use(balanceRouter);
+
+app.listen(port);
